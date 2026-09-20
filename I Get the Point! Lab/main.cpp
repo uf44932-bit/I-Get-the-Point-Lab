85e43ed2398175216ce209da19656b6ec927461f
uf44932-bit <uf44932@mail.harpercollege.edu>
2026-09-20 11:19:28 -0500

    Initial Commit

---
+++ I Get the Point! Lab/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  I Get the Point! Lab
+//
+//  Created by Anahi Munoz on 9/20/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
