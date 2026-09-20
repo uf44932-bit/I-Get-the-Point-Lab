c50cedc6484602aa880b8e64e2a0084fbf599626
uf44932-bit <uf44932@mail.harpercollege.edu>
2026-09-20 11:34:34 -0500

    started

--- I Get the Point! Lab/main.cpp
+++ I Get the Point! Lab/main.cpp
@@ -1,14 +1,33 @@
-//
-//  main.cpp
-//  I Get the Point! Lab
-//
-//  Created by Anahi Munoz on 9/20/26.
-//
+/*
+ Create a class called Point that is initialized with the x- and y-coordinates of a point in a Cartesian coordinate plane. Then, create the following overloaded operators on the class - distance, equality, inequality, midpoint, and output streaming.
+
+ ✅ Acceptance Criteria
+
+ Create a Point class that takes an x and y coordinate in its constructor.
+ Override the - operator on Point so that it calculates the distance between the two points.
+ Override the == operator on Point so that it determines if two points are the same coordinate or different coordinates.
+ Override the != operator on Point so that it determines if two points are not the same coordinate or different coordinates.
+ Override the / operator on Point so that it determines midpoint between the two points.
+ Override the << operator on Point so that a user can stream a point as if it were a C++ string.
+
+ */
+
+
+
+
+
 
 #include <iostream>
+#include <cmath>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+using namespace std;
+
+
+
+
+int main() {
+    
+  
     return EXIT_SUCCESS;
 }
